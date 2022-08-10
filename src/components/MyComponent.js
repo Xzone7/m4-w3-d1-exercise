66de109e617e7fd79477d2315cc70239fbb21266
xu chen <xuchen@xus-MacBook-Pro.attlocal.net>
2022-08-09 20:28:47 -0700

    in class demo

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 export default class MyComponent extends React.Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <input onChange={(e) => {this.setState({ input: e.target.value })}} type="text" />
+      </div>
+    );
   }
 }
--- src/components/MyComponent.test.js
+++ src/components/MyComponent.test.js
@@ -3,7 +3,37 @@ import { shallow } from 'enzyme';
 import MyComponent from './MyComponent';
 
 describe("MyComponent", () => {
+  let component;
+
+  beforeEach(() => {
+    component = shallow(<MyComponent />);
+  });
+
   it("should render correctly", () => {
-    const component = shallow(<MyComponent />);
+    expect(component).not.toBe.null;
+  });
+
+  it("should render initial layout", () => {
+    expect(component.getElement()).toMatchSnapshot();
+  });
+
+  it("should create an entry in component state", () => {
+    component.find("input").props().onChange({
+      target: {
+        name: "myName",
+        value: "myValue"
+      }
+    });
+    expect(component.state("input")).toBeDefined();
+  });
+
+  it("should create an entry in component state with the event value", () => {
+    component.find("input").props().onChange({
+      target: {
+        name: "myName",
+        value: "myValue"
+      }
+    });
+    expect(component.state("input")).toEqual("myValue");
   });
 });
